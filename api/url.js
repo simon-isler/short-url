67f03c0049055773a99d724bc55437d68f4e01bb
simon <simon-isler@gmx.ch>
2021-06-28 16:58:38 +0200

    Count usages

--- api/url.js
+++ api/url.js
@@ -1,4 +1,7 @@
-import { urlRef } from "../src/firebase";
+import { firebase, urlRef } from "../src/firebase";
+
+const increment = firebase.firestore.FieldValue.increment(1);
+let url = process.env.APP_HOST;
 
 function format(url) {
     if (url.startsWith('https://') || url.startsWith('http://')) { return url; }
@@ -8,8 +11,13 @@ function format(url) {
 
 module.exports = async (req, res) => {
     const { query: { id } } = req;
-    const data = (await urlRef.doc(id).get()).data();
-    const url = data?.targetUrl ? data.targetUrl : process.env.APP_HOST;
+    const doc = urlRef.doc(id);
+    const targetUrl = (await doc.get()).data()?.targetUrl;
+
+    if (targetUrl) {
+        await doc.update({ usages: increment });
+        url = targetUrl;
+    }
 
     res.redirect(format(url));
 }
